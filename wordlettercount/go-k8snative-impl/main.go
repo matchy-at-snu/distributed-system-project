7328a2f6b92bf2051a138e9bfffef2f9c21b9b36
matchy233 <matchy233@gmail.com>
2020-12-08 01:58:17 +0900

    Add go-k8snative-impl app skeleton

---
+++ wordlettercount/go-k8snative-impl/main.go
@@ -0,0 +1,23 @@
+package main
+
+// Should follow the canonical split -> map -> sort -> shuffle -> reduce scheme
+
+func main() {
+	// split here
+
+	// feed to mapper here
+
+	// collect from mapper here
+
+	// sort and shuffle here
+
+	// feed to reducer here
+
+	// collect here
+
+	// merge sort here
+
+	// pretty print here
+
+	return
+}
---
+++ wordlettercount/go-k8snative-impl/util/mapper.go
@@ -0,0 +1,5 @@
+package util
+
+func mapper() {
+	return
+}
